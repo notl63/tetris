b61b804c635eb8e772f65c21e5961af04b73f203
notl63 <64993109+notl63@users.noreply.github.com>
2022-04-04 09:22:31 -0700

    first

---
+++ scripts/background-sound.js
@@ -0,0 +1,8 @@
+﻿window.StartPlayingBackgroundMusic = function () {
+	document.getElementById('background-music-audio').play();
+}
+
+window.StopPlayingBackgroundMusic = function () {
+	// It's also possible to reset the time.
+	document.getElementById('background-music-audio').pause();
+}
---
+++ scripts/keyboard-listener.js
@@ -0,0 +1,30 @@
+﻿/*
+ * References:
+ * -----------
+ * https://stackoverflow.com/questions/71242841/listen-to-keyboard-input-in-the-whole-blazor-page/71243751
+ * https://docs.microsoft.com/en-us/aspnet/core/blazor/javascript-interoperability/call-dotnet-from-javascript?view=aspnetcore-6.0#invoke-an-instance-net-method
+*/
+
+window.TetrisFunctions = {
+	addKeyboardListenerEvent: function (listener) {
+		let serializeEvent = function (event) {
+			if (event) {
+				return {
+					key: event.key,
+					code: event.keyCode.toString(),
+					location: event.location,
+					repeat: event.repeat,
+					ctrlKey: event.ctrlKey,
+					shiftKey: event.shiftKey,
+					altKey: event.altKey,
+					metaKey: event.metaKey,
+					type: event.type
+				};
+			}
+		};
+
+		window.document.addEventListener('keydown', function (event) {
+			listener.invokeMethodAsync('BrowserKeyDownReceived', serializeEvent(event))
+		});
+	}
+};
